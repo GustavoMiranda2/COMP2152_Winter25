f353efe6958ce1cf30c6abd98a01bc43f590615e
Gustavo Miranda <101488574@georgebrown.ca>
2025-01-19 23:27:22 -0500

    Lab1 final version whit screenshots

---
+++ lab1/lab1.py
@@ -0,0 +1,20 @@
+# Sample Coding Questions 01 Week 01
+# Name: Gustavo Miranda
+
+Numbers = [1, 4, 7, 9]
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = ((a - ((b ** c) // d)) + (a % c))
+
+temperature = 32.6
+format = "%.3f"
+
+print("The temperature today is:",format % temperature, "degrees Celsius")
+
+
+userAge = int(input("Enter your age: "))
+print("Now showing the shop items filtered by age", userAge)
